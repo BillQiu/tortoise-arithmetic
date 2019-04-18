4e3838239f2c9982ba63cd5cc78bfb08dc54fbdf
Bill Qiu <fanglaiq@gmail.com>
2019-04-18 12:46:49 +0800

    recursion binary search

--- binary-search/index.js
+++ binary-search/index.js
@@ -9,6 +9,7 @@
  * 使用循环的方式实现。
  *
  * @param {array} arr 有序数组
+ * @param {number} target 目标值
  */
 function cyclicBinarySearch(arr, target) {
     if (arr.length <= 1) return arr;
@@ -17,7 +18,7 @@ function cyclicBinarySearch(arr, target) {
     let end = arr.length - 1;
 
     while (start <= end) {
-        const middle = Math.floor(start, end);
+        const middle = Math.floor((start + end) / 2);
 
         if (target === arr[middle]) {
             return middle;
@@ -31,6 +32,30 @@ function cyclicBinarySearch(arr, target) {
     return -1;
 }
 
+/**
+ * 使用递归的方式实现。
+ *
+ * @param {*} arr
+ * @param {*} target
+ */
+function recursionBinarySearch(arr, target) {
+    if (arr.length === 0) return -1;
+
+    let start = 0;
+    let end = arr.length - 1;
+    // const middle = Math.floor((start + end) / 2);
+    const middle = start + ((end - start) >> 1); // 位运算，提升性能；防止数值太大溢出。
+
+    if (target === arr[middle]) {
+        return middle;
+    } else if (target > arr[middle]) {
+        return recursionBinarySearch(arr.splice(middle));
+    } else {
+        return recursionBinarySearch(arr.splice(0, middle - 1));
+    }
+}
+
 module.exports = {
-    cyclicBinarySearch
+    cyclicBinarySearch,
+    recursionBinarySearch
 };
--- binary-search/index.test.js
+++ binary-search/index.test.js
@@ -1,4 +1,4 @@
-const { cyclicBinarySearch } = require("./index");
+const { cyclicBinarySearch, recursionBinarySearch } = require("./index");
 const expect = require("chai").expect;
 
 const source = [8, 11, 19, 23, 27, 33, 45, 55, 67, 98];
@@ -7,6 +7,11 @@ const target = 19;
 describe("BinarySearchTest", function() {
     it("cyclic", function() {
         expect(cyclicBinarySearch(source, target)).to.equal(
+            source.indexOf(target)
+        );
+    });
+    it("recursion", function() {
+        expect(recursionBinarySearch(source, target)).to.equal(
             source.indexOf(target)
         );
     });
