8e9f8770276552ff5020fa9101c46100a75c5154
Bill Qiu <fanglaiq@gmail.com>
2019-05-28 15:09:28 +0800

    nlargest

--- heap/index.js
+++ heap/index.js
@@ -9,7 +9,6 @@ class Heap {
     constructor(arr = []) {
         this.a = [,]; // 从 1 开始计数
         this.count = 0; // 存储数量
-        this.heapify(arr); // 堆化
     }
 
     get heap() {
@@ -36,14 +35,12 @@ class Heap {
         }
     };
 
-    heapify = arr => {
-        if (!arr.length) return;
-    };
-
     insert = data => {
         ++this.count;
+        // 插入
         this.a[this.count] = data;
 
+        // 自下向上堆化
         let i = this.count;
         while (i / 2 > 0 && this.a[i] > this.a[i / 2]) {
             this.swap(i, i / 2, this.a);
@@ -51,7 +48,77 @@ class Heap {
         }
     };
 
-    pop = data => {};
+    pop = data => {
+        if (this.count === 0) {
+            return null;
+        }
+
+        const result = this.a[1];
+
+        this.a[1] = this.a[this.count];
+        this.a.pop();
+        --this.count;
+
+        const heapify = (a, n, i) => {
+            while (true) {
+                let maxPos = i;
+                if (i * 2 <= n && a[i] < a[i * 2]) maxPos = i * 2;
+                if (i * 2 + 1 <= n && a[i] < a[i * 2 + 1]) maxPos = i * 2 + 1;
+                if (maxPos === i) {
+                    break;
+                }
+                this.swap(i, maxPos, this.a);
+                i = maxPos;
+            }
+        };
+
+        heapify(this.a, this.count, 1);
+
+        // 自上向下堆化
+
+        return result;
+    };
+
+    //
+
+    /**
+     * 1. 堆化：从第一个非叶子节点开始，向下堆化。
+     * 2. 排序：类似“删除堆顶元素”的操作。将堆顶元素移除后，把下标为 n 的元素放到堆顶，
+     * 然后重新构建堆。
+     *
+     * @memberof Heap
+     */
+    nlargest = (arr, num) => {
+        if (arr.length === 0) return;
+
+        arr.unshift(null);
+
+        const firstNum = Math.floor(arr.length / 2);
+        const count = arr.length - 1;
+        let firstPos = firstNum;
+
+        const heapify = (a, n, i) => {
+            while (true) {
+                let maxPos = i;
+
+                if (i * 2 <= n && a[i] < a[i * 2]) maxPos = i * 2;
+                if (i * 2 + 1 <= n && a[i] < a[i * 2 + 1]) maxPos = i * 2 + 1;
+                if (maxPos === i) {
+                    break;
+                }
+                this.swap(i, maxPos, a);
+                i = maxPos;
+            }
+        };
+
+        while (true) {
+            heapify(arr, count, firstPos);
+            --firstPos;
+            if (firstPos === 0) break;
+        }
+
+        return arr.slice(1, num + 1);
+    };
 }
 
 module.exports = Heap;
--- heap/index.test.js
+++ heap/index.test.js
@@ -9,4 +9,20 @@ describe("heap", () => {
         heap.insert(1);
         expect(heap.heap).to.deep.equal([3, 2, 1]);
     });
+
+    it("pop", () => {
+        const heap = new Heap();
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(1);
+        const max = heap.pop();
+        expect(max).to.equal(3);
+        expect(heap.heap).to.deep.equal([2, 1]);
+    });
+
+    it("nlargest", () => {
+        const heap = new Heap();
+        const arr = [3, 4, 652, 5, 3];
+        expect(heap.nlargest(arr, 2)).to.deep.equal([652, 5]);
+    });
 });
